84228d354f30d3dedb4487ed82ceafe50a3a2a6a
Carla Holcomb <carla.programmer@gmail.com>
2019-12-18 03:21:39 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -36,11 +36,10 @@ ON users.id = pledges.user_id GROUP BY users.name;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-   "SELECT projects.title, (SUM(pledges.amount) - projects.funding_goal) AS amount_over_goal
+"SELECT projects.category, SUM(pledges.amount)
 FROM projects
 INNER JOIN pledges
-ON projects.id = pledges.project_id GROUP BY projects.title
-    HAVING amount_over_goal >= 0;"
+ON projects.id = pledges.project_id WHERE category = 'music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
